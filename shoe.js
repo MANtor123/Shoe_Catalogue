e5e40374dfdf67e81dc24c07332b3ddff9d11dce
Mponeng <mantombiratego@gmail.com>
2017-04-10 16:02:02 +0200

    updated webpage

---
+++ shoe.js
@@ -0,0 +1,61 @@
+// var button = document.querySelector('#button');
+var display = document.querySelector('.block');
+ var myTemplate = document.querySelector('#myTemplate');
+ var temp = Handlebars.compile(myTemplate.innerHTML);
+// var brand = document.querySelector('.brands');
+// var size = document.querySelector('.size');
+// var color = document.querySelector('.color');
+
+var shoes = [
+        {
+            color : 'white',
+            price : 350,
+            in_stock : 5,
+            brand : 'Nike',
+            size : 4
+        },
+        {
+            color : 'grey',
+            price : 275,
+            in_stock : 3,
+            brand : 'Vans',
+            size : 5
+        },
+
+        {
+          color : 'black',
+          price :345,
+          brand : 'Allstar',
+          size : 6
+        },
+];
+
+var colors = [];
+
+for(var i=0; i<shoes.length; i++){
+  var shoe = shoes[i]
+  colors.push(shoe.color);
+}
+
+var brands = []
+for(var i=0; i<shoes.length; i++){
+  var shoe = shoes[i]
+  brands.push(shoe.brand)
+}
+
+var size = []
+for(var i=0; i<shoes.length; i++){
+  var shoe = shoes[i]
+  size.push(shoe.size)
+}
+
+var results = temp({shoeColorKeys:colors,shoeBrandKeys:brands,shoeSizeKeys:size})
+display.innerHTML = results;
+
+
+
+// button.addEventListener('click',function(){
+//
+//
+//
+// })
