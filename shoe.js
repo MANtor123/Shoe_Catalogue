d200e8f1fa2152c6338418d25821b1c484f46fc3
Mponeng <mantombiratego@gmail.com>
2017-04-13 12:11:43 +0200

    added textbox

--- shoe.js
+++ shoe.js
@@ -4,11 +4,12 @@ var temp = Handlebars.compile(myTemplate.innerHTML);
 var display = document.querySelector('.block');
 
 //second template
-var search = document.querySelector('#button');
+var all = document.querySelector('#button');
 var searchTemplate = document.querySelector('.searchTemplate');
 var searchTemp = Handlebars.compile(searchTemplate.innerHTML);
 var output = document.querySelector('.output');
 
+var add = document.querySelector('#add');
 //list of the shoes
 var shoes = [
         {
@@ -16,7 +17,8 @@ var shoes = [
             price : 350,
             in_stock : 5,
             brand : 'Nike',
-            size : 4
+            size : 4,
+            image : '/home/bootcamp/IMG_8742.JPG'
         },
         {
             color : 'grey',
@@ -40,6 +42,7 @@ var shoes = [
           brand : 'Allstar',
           size : 3,
           in_stock : 7
+
         },
 ];
 
@@ -67,9 +70,48 @@ var results = temp({shoeColorKeys:colors,shoeBrandKeys:brands,shoeSizeKeys:size}
 display.innerHTML = results;
 
 
-
-search.addEventListener('click',function(){
+//all button
+all.addEventListener('click',function(){
 var searchResults = searchTemp({shoes : shoes})
 output.innerHTML = searchResults;
+
+});
+
+//add button
+var stockMap = {}
+add.addEventListener('click', function(){
+var brand = document.querySelector('.brand')
+var color = document.querySelector('.color')
+var sizes = document.querySelector('.size')
+var quantity = document.querySelector('.qty')
+var price = document.querySelector('.price')
+
+var brandName = brand.value
+
+if(brandName !== ''){
+  var name = stockMap[brandName];
+  if(name == undefined){
+    name ={
+      brand : brand.value
+      color : color.value
+      size : sizes.value
+      price : price.value
+      in_stock : Number (quantity.value)
+    };
+    shoes.push(name);
+    stockMap[brandName] = name
+  }
+  else{
+      name.qty += Number(qty.value)
+  }
+}
+var searchResults = searchTemp({shoes : shoes})
+output.innerHTML = searchResults;
+});
+
+
+
+
+
 
 });
